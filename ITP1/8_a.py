743318f00b26031db6bb783f4e3e61445b66f9d4
Riku <smile6280@icloud.com>
2021-07-19 22:21:00 +0900

    Add 8_a

---
+++ ITP1/8_a.py
@@ -0,0 +1,13 @@
+# a = str(input())
+# b = str()
+
+# for s in a:
+#     if s.isalpha() == True and s.islower == True:
+#         b += s.upper()
+#     elif s.isalpha() == True and s.isupper == True:
+#         b += s.lower()
+#     elif s.isalpha == False:
+#         b += s
+# print(b)
+
+print(input().swapcase())
